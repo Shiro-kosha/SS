8574ee9c39b22ac874e356b5c30102bf434ad705
a499209 <83036671+a499209@users.noreply.github.com>
2025-09-19 20:00:51 +0200

    1

--- gallery/Gallery.gd
+++ gallery/Gallery.gd
@@ -56,24 +56,25 @@ func iload(path):
 
 
 func fill():
+	report("Filling...")
 	var dir = Directory.new()
 	for i in DB.gallery.keys():
 		var path = str("res://gallery/arts/", i, ".jpg")
-		if dir.file_exists(path):
-			var img = iload(path)
-			var img_size = img.get_size()
-			var TR = TextureRect.new()
-			var col = col_right
-			if img_size.x > img_size.y:
-				col = col_mid
-			elif col_left.get_child_count() <= col_right.get_child_count():
-				col = col_left
-				
-			TR.set("texture", img)
-			TR.set("expand", true)
-			var msy = (img_size.y * col.rect_size.x) / img_size.x 
-			TR.rect_min_size.y = msy
-			col.add_child(TR)
+#		if dir.file_exists(path):
+		var img = iload(path)
+		var img_size = img.get_size()
+		var TR = TextureRect.new()
+		var col = col_right
+		if img_size.x > img_size.y:
+			col = col_mid
+		elif col_left.get_child_count() <= col_right.get_child_count():
+			col = col_left
+			
+		TR.set("texture", img)
+		TR.set("expand", true)
+		var msy = (img_size.y * col.rect_size.x) / img_size.x 
+		TR.rect_min_size.y = msy
+		col.add_child(TR)
 		yield(get_tree().create_timer(0.1), "timeout")
 		return
 
